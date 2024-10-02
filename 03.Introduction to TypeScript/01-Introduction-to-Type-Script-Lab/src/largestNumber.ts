369a9fdc4e5d5227e32955364c02fd09546e94d1
Todor Krumov <baito6u@gmail.com>
2024-10-02 06:01:22 +0300

    Create largestNumber.ts

---
+++ 03.Introduction to TypeScript/01-Introduction-to-Type-Script-Lab/src/largestNumber.ts
@@ -0,0 +1,6 @@
+function largestNumber(a: number, b: number, c: number): void {
+    const largestNum = Math.max(a, b, c);
+    console.log(`The largest number is ${largestNum}.`);
+}
+
+largestNumber(5, -3, 16);
