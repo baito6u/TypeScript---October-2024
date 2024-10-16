18d4c03a4deacde7a83bd0997548cc4f892e2a0f
Todor Krumov <baito6u@gmail.com>
2024-10-16 06:09:46 +0300

    Update carDealership.ts

--- 04.Generics/Generics-Lab/src/carDealership.ts
+++ 04.Generics/Generics-Lab/src/carDealership.ts
@@ -7,3 +7,43 @@ interface Action<T> {
     sellCar(dealerID: T, model: T): void;
 }
 
+class CarDealership<T> implements Dealership<T>, Action<T> {
+    public modelsSold: { [key: string]: T }; // Store dealerID as key and model as value
+    
+    dealershipName: T;
+    numberOfSoldCars: number;
+
+    constructor(dealershipName: T) {
+        this.dealershipName = dealershipName;
+        this.numberOfSoldCars = 0;
+        this.modelsSold = {};
+
+    }
+
+    sellCar(dealerID: T, model: T): void {
+        this.modelsSold[dealerID as unknown as string] = model;
+        this.numberOfSoldCars += 1;
+    }
+
+    showDetails(): string {
+        let result = `${this.dealershipName}:\n`
+
+        for (let dealerID in this.modelsSold) {
+            result += `${dealerID} sold ${this.modelsSold[dealerID]}\n`;
+        }
+
+        return result.trim();
+    }
+    
+}
+
+let dealership = new CarDealership<string>('SilverStar');
+
+// Selling cars
+dealership.sellCar('BG01', 'C Class');
+dealership.sellCar('BG02', 'S Class');
+dealership.sellCar('BG03', 'ML Class');
+dealership.sellCar('BG04', 'CLK Class');
+
+// Output the details
+console.log(dealership.showDetails());
