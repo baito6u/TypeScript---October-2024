42b2f94b390487754fe9a83a3dae11361d433cc1
Todor Krumov <baito6u@gmail.com>
2024-10-20 07:24:13 +0300

    Update main.ts

--- 07.Workshop/workshop/src/main.ts
+++ 07.Workshop/workshop/src/main.ts
@@ -1,31 +1,26 @@
+import { PostsService, Post } from './services/PostsService';
+import { UsersService, User } from './services/UsersService';
 import { HtmlRenderer } from './renderers/HtmlRenderer';
-import { Post, PostsService } from './services/PostsService'
-import { User, UsersService } from './services/UsersService';
-import './style.css'
 
 const postsService = new PostsService();
 const usersService = new UsersService();
-const renderer = new HtmlRenderer('app');
+const postsRenderer = new HtmlRenderer<Post>('app');
+const usersRenderer = new HtmlRenderer<User>('app');
 
+// Function to render posts
 async function renderPosts() {
-  const posts: Post[] = await postsService.getAllPosts();
-  let content = '<h1>Posts</h1>';
-
-  posts.forEach((post) => {
-    content += `<h2>${post.title}</h2><p>${post.body}</p>`;
-  });
-  renderer.render(content);
+  const posts: Post[] = await postsService.getAll();
+  postsRenderer.render(posts, (post) => `<h2>${post.title}</h2><p>${post.body}</p>`);
 }
 
+
+
+// Function to render users
 async function renderUsers() {
-  const users: User[] = await usersService.getAllUsers();
-  let content = '<h1>Users</h1>';
-  users.forEach((user) => {
-    content += `<h2>${user.name}</h2><p>${user.email}</p>`;
-  });
-  renderer.render(content);
+  const users: User[] = await usersService.getAll();
+  usersRenderer.render(users, (user) => `<h2>${user.name}</h2><p>${user.email}</p>`);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   renderUsers();
-})
+});
