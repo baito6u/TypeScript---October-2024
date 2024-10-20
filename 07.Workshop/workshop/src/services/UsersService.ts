5272c04f447f8fa292af260cf6a7471aad9f53c4
Todor Krumov <baito6u@gmail.com>
2024-10-20 07:24:40 +0300

    Update UsersService.ts

--- 07.Workshop/workshop/src/services/UsersService.ts
+++ 07.Workshop/workshop/src/services/UsersService.ts
@@ -1,3 +1,5 @@
+import { GenericService } from './GenericService';
+
 export interface User {
   id: number;
   name: string;
@@ -5,44 +7,9 @@ export interface User {
   email: string;
 }
 
-export class UsersService {
-  private apiUrl: string = "https://jsonplaceholder.typicode.com/users";
-
-  async getAllUsers(): Promise<User[]> {
-    const responce = await fetch(this.apiUrl);
-    return responce.json();
-  }
-
-  async getUserByID(id: number): Promise<User> {
-    const responce = await fetch(`${this.apiUrl}/${id}`);
-    return responce.json();
-  }
-
-  async createUser(user: User): Promise<User> {
-    const response = await fetch(this.apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    return response.json();
-  }
-
-  async updateUser(id: number, user: Partial<User>): Promise<User> {
-    const response = await fetch(`${this.apiUrl}/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    return response.json();
-  }
-
-  async deleteUser(id: number): Promise<void> {
-    await fetch(`${this.apiUrl}/${id}`, {
-      method: 'DELETE',
-    });
+export class UsersService extends GenericService<User> {
+  constructor() {
+    super('https://jsonplaceholder.typicode.com/users');
   }
 }
+
