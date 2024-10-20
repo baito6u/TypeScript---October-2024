2c77af1e41ede1f106eb55fbe0ab9b12e45849f6
Todor Krumov <baito6u@gmail.com>
2024-10-20 06:40:04 +0300

    Update main.ts

--- 07.Workshop/workshop/src/main.ts
+++ 07.Workshop/workshop/src/main.ts
@@ -1,8 +1,31 @@
+import { HtmlRenderer } from './renderers/HtmlRenderer';
+import { Post, PostsService } from './services/PostsService'
+import { User, UsersService } from './services/UsersService';
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Workshop Vite + TypeScript</h1>
-  </div>
-`
+const postsService = new PostsService();
+const usersService = new UsersService();
+const renderer = new HtmlRenderer('app');
 
+async function renderPosts() {
+  const posts: Post[] = await postsService.getAllPosts();
+  let content = '<h1>Posts</h1>';
+
+  posts.forEach((post) => {
+    content += `<h2>${post.title}</h2><p>${post.body}</p>`;
+  });
+  renderer.render(content);
+}
+
+async function renderUsers() {
+  const users: User[] = await usersService.getAllUsers();
+  let content = '<h1>Users</h1>';
+  users.forEach((user) => {
+    content += `<h2>${user.name}</h2><p>${user.email}</p>`;
+  });
+  renderer.render(content);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderUsers();
+})
