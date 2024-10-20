6561716d02b39bf5f275da6dc8437f0912c29781
Todor Krumov <baito6u@gmail.com>
2024-10-20 07:52:55 +0300

    Update router.ts

--- 07.Workshop/workshop/src/router.ts
+++ 07.Workshop/workshop/src/router.ts
@@ -1,33 +1,31 @@
-// src/router.ts
-type RouteConfig = {
-    path: string;
-    component: () => void;
-  };
-  
-  export class Router {
-    private routes: RouteConfig[];
-  
-    constructor() {
-      this.routes = [];
-      window.addEventListener("hashchange", () => this.routeChanged());
-    }
-  
-    addRoute(path: string, component: () => void) {
-      this.routes.push({ path, component });
-    }
-  
-    start() {
-      this.routeChanged(); // Handle initial load
-    }
-  
-    private routeChanged() {
-      const currentHash = window.location.hash.slice(1);
-      const route = this.routes.find((r) => r.path === currentHash);
-      if (route) {
-        route.component();
-      } else {
-        console.error("Route not found!");
-      }
+type RouteConfig<T> = {
+  path: string;
+  component: () => T;
+};
+
+export class Router<T> {
+  private routes: RouteConfig<T>[];
+
+  constructor() {
+    this.routes = [];
+    window.addEventListener("hashchange", () => this.routeChanged());
+  }
+
+  addRoute(path: string, component: () => T) {
+    this.routes.push({ path, component });
+  }
+
+  start() {
+    this.routeChanged(); // Handle initial load
+  }
+
+  private routeChanged() {
+    const currentHash = window.location.hash.slice(1);
+    const route = this.routes.find((r) => r.path === currentHash);
+    if (route) {
+      route.component();
+    } else {
+      console.error("Route not found!");
     }
   }
-  
+}
