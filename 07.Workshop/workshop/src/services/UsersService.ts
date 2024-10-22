293439c163c73f5c2849d4ead31aeaa922a72a37
Todor Krumov <baito6u@gmail.com>
2024-10-22 23:17:31 +0300

    Update UsersService.ts

--- 07.Workshop/workshop/src/services/UsersService.ts
+++ 07.Workshop/workshop/src/services/UsersService.ts
@@ -1,3 +1,5 @@
+import { User } from "../types/User";
+
 export class UsersService {
   private apiUrl: string;
 
@@ -5,7 +7,7 @@ export class UsersService {
     this.apiUrl = 'https://jsonplaceholder.typicode.com/users';
   }
 
-  async fetchUsers(): Promise<any[]> {
+  async fetchUsers(): Promise<User[]> {
     const response = await fetch(this.apiUrl);
     if (!response.ok) {
       throw new Error('Failed to fetch users');
