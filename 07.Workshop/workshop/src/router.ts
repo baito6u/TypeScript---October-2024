b7e5765fb046d557c207c5f58fefc02a3ec2aef3
Todor Krumov <baito6u@gmail.com>
2024-10-22 22:29:45 +0300

    Update router.ts

--- 07.Workshop/workshop/src/router.ts
+++ 07.Workshop/workshop/src/router.ts
@@ -1,4 +1,3 @@
-// src/router.ts
 type RouteHandler = () => void;
 
 export class Router {
@@ -9,14 +8,14 @@ export class Router {
   }
 
   start(): void {
-    window.addEventListener('hashchange', () => {
+    window.addEventListener("hashchange", () => {
       const hash = window.location.hash.substring(1);
       const routeHandler = this.routes[hash];
       if (routeHandler) {
         routeHandler();
       }
     });
-    
+
     // Trigger the route based on the initial URL hash
     const initialHash = window.location.hash.substring(1);
     const initialRouteHandler = this.routes[initialHash];
